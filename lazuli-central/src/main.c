69570fa176655140f85be9afc794aa63160dd3dc
Freireg <freireg1503@gmail.com>
2025-03-15 13:03:58 -0300

    Basic html page working

--- lazuli-central/src/main.c
+++ lazuli-central/src/main.c
@@ -18,18 +18,44 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(net_http_server_sample, LOG_LEVEL_DBG);
 
+// HTTPS structures
+
+static uint8_t index_html_gz[] = {
+#include "index.html.gz.inc"
+};
+
+static struct http_resource_detail_static index_html_gz_resource_detail = {
+	.common = {
+			.type = HTTP_RESOURCE_TYPE_STATIC,
+			.bitmask_of_supported_http_methods = BIT(HTTP_GET),
+			.content_encoding = "gzip",
+			.content_type = "text/html",
+		},
+	.static_data = index_html_gz,
+	.static_data_len = sizeof(index_html_gz),
+};
+
 // Server configuration
 static uint16_t test_http_service_port = CONFIG_NET_HTTP_SERVER_SERVICE_PORT;
 HTTP_SERVICE_DEFINE(test_http_service, NULL, &test_http_service_port, 1,
 		    10, NULL, NULL);
 
+HTTP_RESOURCE_DEFINE(index_html_gz_resource, test_http_service, "/",
+		     &index_html_gz_resource_detail);
+
+/* -------------------------------------------------------- */
 // Functions prototypes
-static void setup_tls(void);
+
+
+/* -------------------------------------------------------- */
+
 
 int main(void){
   int ret = 0;
 
+  //Initialize this network application
   net_config_init_app(NULL, "Initializing Network");
+  //Start the HTTP2 server
   http_server_start();
 
   return 0;
